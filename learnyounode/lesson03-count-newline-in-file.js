255f409a5959979ade608eb657ec5826df364a7b
Ravi K Sinha <ravisinha@in.unibiz.com>
2018-01-23 22:31:23 +0530

    Lesson 3 - node - counting newlines in a text file

---
+++ learnyounode/lesson03-count-newline-in-file.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+
+console.log(fs.readFileSync(process.argv[2]).toString().split('\n').length - 1);
+
+/* Official 
+ var fs = require('fs')
+
+ var contents = fs.readFileSync(process.argv[2])
+ var lines = contents.toString().split('\n').length - 1
+ console.log(lines)
+
+ // note you can avoid the .toString() by passing 'utf8' as the
+ // second argument to readFileSync, then you'll get a String!
+ //
+ // fs.readFileSync(process.argv[2], 'utf8').split('\n').length - 1
+
+*/
