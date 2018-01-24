1716312d4fa1c8f7063e663db93601805b8f1e2e
Ravi K Sinha <ravisinha@in.unibiz.com>
2018-01-24 12:34:10 +0530

    Lesson 10 - Node - Time Server , return formatted date

---
+++ learnyounode/lesson10-time-server.js
@@ -0,0 +1,45 @@
+var net = require('net');
+
+var server = net.createServer(function(socket) {
+    socket.end(getFormattedDate());
+});
+
+function getFormattedDate() {
+    var date = new Date();
+    return date.getFullYear()
+        + '-'
+        + date.getMonth() + 1
+        + '-'
+        + date.getDate()
+        + ' '
+        + date.getHours()
+        + ':'
+        + date.getMinutes()
+        + '\n';
+}
+
+server.listen(process.argv[2]);
+
+/* 
+
+var net = require('net')
+
+function zeroFill (i) {
+  return (i < 10 ? '0' : '') + i
+}
+
+function now () {
+  var d = new Date()
+  return d.getFullYear() + '-' +
+    zeroFill(d.getMonth() + 1) + '-' +
+    zeroFill(d.getDate()) + ' ' +
+    zeroFill(d.getHours()) + ':' +
+    zeroFill(d.getMinutes())
+}
+
+var server = net.createServer(function (socket) {
+  socket.end(now() + '\n')
+})
+
+server.listen(Number(process.argv[2]))
+*/
